647c8fa057cb913a452e87e2251b55be1f2aa175
hamada <hamadagh777@gmail.com>
2020-09-20 17:59:37 +0200

    fetching weather data

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import SideBar from "./components/SideBar";
 import "./App.css";
 
 const getWeatherData = async () => {
-  const key = process.env.API_KEY;
+  const city = "hamburg";
+  const key = process.env.REACT_APP_API_KEY;
   const res = await fetch(
-    `api.openweathermap.org/data/2.5/forecast?q={berlin}&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`
   );
   return res.json();
 };
 
 function App() {
   const { data, status } = useQuery("weatherData", getWeatherData);
+  console.log(data);
+
   return (
     <div className="App">
       <Main />
