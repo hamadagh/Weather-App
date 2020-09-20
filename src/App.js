4b655bc29f87a0e0ec80a4c8c639d30fabbcd743
hamada <hamadagh777@gmail.com>
2020-09-20 17:13:57 +0200

    adding react-query and get weather data function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useQuery } from "react-query";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 import "./App.css";
 
+const getWeatherData = async () => {
+  const key = process.env.API_KEY;
+  const res = await fetch(
+    `api.openweathermap.org/data/2.5/forecast?q={berlin}&appid=${key}`
+  );
+  return res.json();
+};
+
 function App() {
+  const { data, status } = useQuery("weatherData", getWeatherData);
   return (
     <div className="App">
       <Main />
