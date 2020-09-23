55e49b0a045f80d0da7f0b934f1dafea21c99401
hamada <hamadagh777@gmail.com>
2020-09-23 13:16:30 +0200

    adding week-days to NavDays components

--- src/components/DaysNav.js
+++ src/components/DaysNav.js
@@ -11,13 +11,34 @@ function DaysNav({ setData }) {
 
   return (
     <div className="days-nav">
-      <span onClick={handleClick}>Mon</span>
-      <span>Tue</span>
-      <span>Wed</span>
-      <span>Thu</span>
-      <span>Fri</span>
-      <span>Sat</span>
-      <span>Sun</span>
+      <div className="week-days">
+        <span>Mon</span>
+        <span>4</span>
+      </div>
+      <div className="week-days">
+        <span>Tue</span>
+        <span>2</span>
+      </div>
+      <div className="week-days">
+        <span>Wed</span>
+        <span>1</span>
+      </div>
+      <div className="week-days">
+        <span>Thu</span>
+        <span>4</span>
+      </div>
+      <div className="week-days">
+        <span>Fri</span>
+        <span>6</span>
+      </div>
+      <div className="week-days">
+        <span>Sat</span>
+        <span>8</span>
+      </div>
+      <div className="week-days">
+        <span>Sun</span>
+        <span>10</span>
+      </div>
     </div>
   );
 }
