ba80570ef8bde59407e61dc8f2898fd132a67331
hamada <hamadagh777@gmail.com>
2020-10-05 18:30:39 +0200

    adding icons

--- src/components/DayDetails.js
+++ src/components/DayDetails.js
@@ -2,6 +2,35 @@ import React, { useContext } from "react";
 import DataContext from "../context/DataContext";
 import "../App.css";
 
+const thunderStormCode = [200, 201, 202, 230, 231, 232, 233];
+const cloudyCode = [611, 612];
+const partlyCloudyCode = [700, 711, 721, 731, 741, 751, 803, 804];
+const partlyRainy = [521];
+const partlySnowy = [600, 621];
+const rainyCode = [300, 301, 302, 500, 501, 502, 511, 520, 522, 900];
+const snowyCode = [601, 602, 610, 622, 623];
+
+let iconClass;
+const setIconCode = (iconCode) => {
+  if (thunderStormCode.includes(iconCode)) {
+    return (iconClass = "thunder-storm");
+  } else if (cloudyCode.includes(iconCode)) {
+    return (iconClass = "cloudy");
+  } else if (partlyCloudyCode.includes(iconCode)) {
+    return (iconClass = "partly-cloudy");
+  } else if (partlyRainy.includes(iconCode)) {
+    return (iconClass = "partly-rainy");
+  } else if (partlySnowy.includes(iconCode)) {
+    return (iconClass = "partly-snowy");
+  } else if (rainyCode.includes(iconCode)) {
+    return (iconClass = "rainy");
+  } else if (snowyCode.includes(iconCode)) {
+    return (iconClass = "snowy");
+  } else {
+    return (iconClass = "clear-sky");
+  }
+};
+
 function DayDetails({ value }) {
   const { data } = useContext(DataContext);
 
@@ -20,12 +49,14 @@ function DayDetails({ value }) {
   const slicedWeekDays2 = weekDays.slice(0, day);
   const finalDays = slicedWeekDays1.concat(slicedWeekDays2);
 
+  const iconCode = data.data.data[value].weather.code;
+
+  setIconCode(iconCode);
+
   if (data) {
-    console.log(data);
     const dayTemp = data.data.data[value].temp;
     const weatherStatus = data.data.data[value].weather.description;
-    const iconCode = data.data.data[value].weather.icon;
-    console.log(iconCode);
+
     return (
       <div className="container">
         <div className="weather-details">
@@ -38,10 +69,7 @@ function DayDetails({ value }) {
             <span className="weather-status">{weatherStatus}</span>
           </div>
         </div>
-        <div
-          className="icons"
-          style={{ backgroundImage: `url(./img/icons/${iconCode}.png)` }}
-        ></div>
+        <div className={`icons ${iconClass}`} id="icons"></div>
       </div>
     );
   } else {
